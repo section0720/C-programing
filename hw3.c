66322c4e7530fa8040116d8cfb129572139eb4a3
section0720 <40137249+section0720@users.noreply.github.com>
2023-10-03 01:04:02 +0900

    hw3.c

--- hw3.c
+++ hw3.c
@@ -21,27 +21,3 @@ int main(void)
 	}
 	return 0;
 }
-
-
-/*
-int main(void)
-{
-	int a;
-	int b;
-	int c;
-
-	for (a = 0; a < 5; a++)
-	{
-		for (b = a; b <= 5 - 1; b++) {
-			printf(" ");
-		}
-		for (c = 0; c <= (a * 2); c++) {
-			printf("*");
-		}
-		printf("\n");
-	}
-
-
-	return 0;
-}
-*/
