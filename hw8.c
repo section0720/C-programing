6add09405b70c93de550a822cda9f76345b3050f
section0720 <40137249+section0720@users.noreply.github.com>
2023-11-23 12:59:02 +0900

    Add files via upload

---
+++ hw8.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+double Sigma(double *param)
+{
+	double sum = 0;
+	double dev = 0;
+	double var = 0;
+	for (int i = 0; i < 5; i++)
+		sum += param[i];
+	double avg = sum / 5;
+	for (int j = 0; j < 5; j++)
+		param[j] -= avg;
+	
+	for (int k = 0; k < 5; k++)
+		dev += param[k];
+	var = pow(dev, 2) / 5;
+	
+	printf("Standard Deviation = %.4lf", sqrt(var));
+	return 0;
+}
+
+int main()
+{
+	int n = 0;
+	double arr[5];
+	printf("Enter 5 real numbers: ");
+	for (n = 0; n < 5; n++)
+	{
+		scanf("%lf", &arr[n]);
+	}
+	Sigma(arr);
+
+}
